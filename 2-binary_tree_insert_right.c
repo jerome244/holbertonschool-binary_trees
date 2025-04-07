30503043a690941a06450f51be98ed0ee009e08c
dougd0ug <dorine.sgv@hotmail.fr>
2025-04-07 12:25:43 +0200

    feat: insert a node as the right-child

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,7 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * *binary_tree_insert_right -  inserts a node as the right-child of another node
+ * *binary_tree_insert_right - inserts a node as the right-child 
+ * of another node
  * Return: binary tree node
  * @parent: parent of new node
  * @value: value of new node
