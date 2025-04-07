a0b61f6d2399115668701427f25546fa508d26ed
root <10485@holbertonstudents.com>
2025-04-07 08:29:35 +0000

    fix: segmentation fault bc of malloc

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -11,8 +11,12 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
 
+	new = malloc(sizeof(binary_tree_t));
 	if (!new)
+	{
+		free(new);
 		return (NULL);
+	}
 	new->n = value;
 	new->parent = parent;
 	new->left = NULL;
