ffbaf8f4936210981e114d0cf6b5658c5adb49b9
dougd0ug <dorine.sgv@hotmail.fr>
2025-04-08 22:25:04 +0200

    fix: with fonction 9

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -30,7 +30,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	}
 	else
 	{
-		if (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right))
+		if (binary_tree_is_perfect(tree->left)
+		&& binary_tree_is_perfect(tree->right))
 		{
 			return (1);
 		}
