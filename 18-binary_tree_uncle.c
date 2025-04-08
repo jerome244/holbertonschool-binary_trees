52dc6d91c03646cf39e5562d887745f72b68de56
dougd0ug <dorine.sgv@hotmail.fr>
2025-04-08 22:29:17 +0200

    fix: npde->parent

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -9,8 +9,13 @@
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
 	if (!node || !node->parent || !node->parent->parent)
+	{
 		return (NULL);
-	if (node->parent->parent->left == node)
+	}
+	if (node->parent->parent->left == node->parent)
+	{
 		return (node->parent->parent->right);
+	}
+
 	return (node->parent->parent->left);
 }
